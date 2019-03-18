9d68ee863609f2c3186d07bea73f19bc5e4cdba5
Chris Luedtke <chrisluedtke@gmail.com>
2019-03-18 14:12:53 -0500

    added bs4

--- setup.py
+++ setup.py
@@ -3,13 +3,20 @@ from setuptools import setup, find_packages
 
 README = (Path(__file__).parent / "README.md").read_text()
 
+REQUIRED = [
+        "bs4",
+        "pandas",
+        "numpy"
+]
+
 setup(
     name='clued',
     version='0.1',
     description="Personal library of Python tools",
     long_description=README,
     long_description_content_type="text/markdown",
+    url = "https://github.com/chrisluedtke/clued",
     packages=find_packages(),
     author="Chris Luedtke",
-    install_requires=["bs4"],
+    install_requires=REQUIRED,
 )
